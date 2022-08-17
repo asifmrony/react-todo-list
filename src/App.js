06e06365608d47fb13848b4b109a4b4c8aafcd9c
amrony.codesmith <amrony.codesmith@gmail.com>
2022-08-18 01:08:53 +0600

    UI design mostly done

--- src/App.js
+++ src/App.js
@@ -22,6 +22,18 @@ function App() {
       title: "AddBaji",
       date: "Aug 16th at 7:00 am",
       remainder: true,
+    },
+    {
+      id: 3,
+      title: "AddBaji",
+      date: "Aug 16th at 7:00 am",
+      remainder: true,
+    },
+    {
+      id: 3,
+      title: "AddBaji",
+      date: "Aug 16th at 7:00 am",
+      remainder: true,
     }
   ];
 
@@ -31,10 +43,25 @@ function App() {
       <Header />
       <div className="main p-5 min-h-screen app-main">
         {/* blank todo add button */}
-        <TodoAdd />
-        <div className="todos space-y-4">
+        {todos.length < 1 && <TodoAdd />}
+        <form action="#">
+          <input type="text" name="tasktitle" id="taskTitle" className='mb-4' />
+          <input type="text" name="taskdate" id="taskDate" />
+          <div>
+            <label htmlFor="taskReminder">
+              <input type="checkbox" name="taskremainder" id="taskReminder" className='mr-2'/>
+              Turn on Reminder
+            </label>
+          </div>
+        </form>
+        <div className="todos space-y-4 h-[383px] max-h-[574px] overflow-x-hidden overflow-y-auto pr-4">
           <Todos todos = {todos}/>          
         </div>
+        <div className="mt-5 w-full h-[1px] bg-gray-700"></div>
+        {/* FInished Todos will be here */}
+        {/* <div className="finished-todos space-y-4 overflow-x-hidden overflow-y-auto">
+          <Todos todos = {todos}/>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/Todo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IoIosNotificationsOutline } from 'react-icons/io'
+import { IconContext } from 'react-icons'
+
+function Todo({ todo }) {
+  return (
+    <div className="todo shadow-lg rounded-lg py-3 px-7 flex justify-between items-center border border-gray-400 mt-4">
+        <div className="todo__content flex items-center space-x-7">
+            <input className='w-5 h-5' type="checkbox" name="" id="" />
+            <div>
+                <h3>{todo.title}</h3>
+                <p>{todo.date}</p>
+            </div>
+        </div>
+        <div>
+            <IconContext.Provider value={{ className: "text-3xl bg-gray-600 rounded-full color-white cursor-pointer hover:bg-[#005CC8] hover:ring-offset-2 hover:ring-offset-[#005CC8] ring-2 ring-[#005CC8]"}}>
+                <IoIosNotificationsOutline />
+            </IconContext.Provider>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
